47fbaebe67a6174c26da8847a97aedc3f41952ae
Cheney Pan <somnus0208@gmail.com>
2018-02-09 07:53:37 +0900

    Add comment

--- test.hs
+++ test.hs
@@ -9,55 +9,64 @@ type StateWithOutput = (State,[Int])
 type TrellisGraph    = [(StateWithInput,StateWithOutput)]
 type Polynomial      = [Int]
 
+-- delete the last element from the list
 dl :: [a] -> [a]
 dl (x:[])   = []
 dl (x:xs) = x:dl xs 
 
+-- right shift the state and add one bit
 add :: Binary -> State -> State
 add i xs = i:dl xs 
 
+-- exclusive or on all the bits
 xorl :: [Binary] -> Binary
 xorl [x]      = x
 xorl (x:y:[]) = xor x y
 xorl (x:xs)   = xor x (xorl xs)
 
+-- exclusive or on all the bits assigned by the polynomial
 xorlp :: [Binary] -> Polynomial -> Int
 xorlp xs is = xorl [xs !! i | i <- is] 
 
+-- generate all states using permutation
 gs :: Int -> [State]
 gs 0 = [[]]
 gs n = [0:p | p <- gs (n - 1)] ++ [1:p | p <- gs (n - 1)]
 
+-- generate the number of registers from the state of states
 gsn :: Int -> Int
 gsn n = gsn' n 1 
-    
+
 gsn' :: Int -> Int -> Int 
 gsn' n x = if (2 ^ x) == n then x - 1 else gsn' n (x+1)
 
+-- generate all states with input
 gaswi :: Int -> [StateWithInput]
 gaswi n = zip (gs n) [0,0..] ++ zip (gs n) [1,1..]
 
+-- generate all states with output
 gswo :: StateWithInput -> [Polynomial] -> StateWithOutput
 gswo (s, i) ps = let newState = (add i s)
                  in  (newState, [xorlp newState p | p <- ps])
 
--- input: -Polynomial for each ouput
---        -number of register 
-tl :: [Polynomial] -> Int -> TrellisGraph
+-- generate the trellis graph with input of polynomials and number of registers
+tl :: [Polynomial] -> Int -> TrellisGraph 
 tl ps l = [(swi, gswo swi ps) | swi <- gaswi l]
 
--- input: - Trellis Graph
+-- branch metrics
 bm :: TrellisGraph -> State -> State -> RawCode -> Int
 bm t si so i = let o = head [o_ |((si_,i_),(so_,o_)) <- t,si_==si,so_==so]
                in  length $ i \\ o
 
+-- path metric
 pm :: TrellisGraph ->State -> [RawCode] -> Int
-pm t s [] = if all (\x -> x==0) s then 0 else 9999
+pm t s [] = if all (\x -> x==0) s then 0 else 99999
 pm t s i  = let [a,b] =[si |((si,i),(so,o)) <- t,so==s]
                 fi    = dl i
             in minimum [pm t a fi + bm t a s (last i), 
                         pm t b fi + bm t b s (last i)]
 
+-- encode
 ec :: TrellisGraph -> RawCode -> [RawCode]
 ec t cs = ec' t  [n-n| n<-[1..gsn $ length t]] cs
 
@@ -66,6 +75,7 @@ ec' t s []     = []
 ec' t s (i:is) = let (sn,o) =head [swo|((si_,i_),swo) <- t,si_==s,i_== i]
                  in  o:ec' t sn is
 
+-- decode
 dc :: TrellisGraph -> [RawCode] -> RawCode
 dc t cs = dc' t [n-n| n<-[1..gsn $ length t]] [(take n cs) | n <- [1..length cs]]
 
