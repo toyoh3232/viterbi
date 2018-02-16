4bb7b0ed9f551089f243f3e9b2f9fd3f2db986ea
Cheney Pan <somnus0208@gmail.com>
2018-02-16 17:00:58 +0900

    Correct decoding algrithm

--- test.hs
+++ test.hs
@@ -24,6 +24,12 @@ xorl [x]      = x
 xorl (x:y:[]) = xor x y
 xorl (x:xs)   = xor x (xorl xs)
 
+-- or on all the bits
+orl :: [Bit] -> Bit
+orl [x]      = x
+orl (x:y:[]) = xor x y
+orl (x:xs)   = xor x (xorl xs)
+
 -- exclusive or on all the bits assigned by the polynomial
 xorlp :: [Bit] -> Polynomial -> Bit
 xorlp xs is = xorl [xs !! i | i <- is] 
@@ -59,12 +65,15 @@ bm t si so i = let o = head [o' | ((si', i'), (so', o')) <- t, si' == si, so' ==
                in  length $ i \\ o
 
 -- path metric
-pm :: TrellisGraph ->State -> [Bits] -> Int
-pm t s [] = if all (\x -> x == 0) s then 0 else 99999
-pm t s i  = let [a, b] = [si | ((si, i), (so, o)) <- t, so == s]
-                fi     = dl i
-            in minimum [pm t a fi + bm t a s (last i), 
-                        pm t b fi + bm t b s (last i)]
+pm :: TrellisGraph ->State -> [Bits] -> (State, Int)
+pm t s [] = if all (\x -> x == 0) s then (s,0) else (s,99999)
+pm t s bs  = let [a, b]   = [si | ((si, i), (so, o)) <- t, so == s]
+                 bs'      = dl bs
+                 (sp,i)   = pm t a bs'
+                 (sp',i') = pm t b bs'
+                 j        = i + bm t a s (last bs)
+                 j'       = i' + bm t b s (last bs)
+            in if j < j'  then (a,j) else (b, j')
 
 -- encode
 ec :: TrellisGraph -> Bits -> [Bits]
@@ -77,15 +86,15 @@ ec' t s (i:is) = let (sn, o) = head [swo | ((si', i'), swo) <- t, si' == s,i' ==
 
 -- decode
 dc :: TrellisGraph -> [Bits] -> Bits
-dc t cs = dc' t [n-n | n<-[1..gsn $ length t]] [(take n cs) | n <- [1..length cs]]
+dc t bs = let n      = gsn $ length t
+              (s, _) = head $ sortBy (\(sa,(sa',i)) (sb,(sb',i')) -> compare i i') [(s, pm t s bs) |s <- gs n]
+          in dc' t s bs
 
-dc' :: TrellisGraph -> State -> [[Bits]] -> Bits
-dc' t s []     = []
-dc' t s (c:cs) = let s0         = head [(so, i) | ((si, i),(so, o)) <- t,si == s,i == 0]
-                     s1         = head [(so, i) | ((si, i),(so, o)) <- t,si == s,i == 1]
-                     (s',i,_)   = head $ sortBy (\(s, i, p) (s', i', p') -> compare p p') 
-                                                [(s', i', pm t s' c) | (s', i') <- [s0, s1]]
-                 in i : (dc' t s' cs)
+dc' :: TrellisGraph -> State -> [Bits] -> Bits
+dc' t s [] = []
+dc' t s bs = let (sp, _) = pm t s bs
+                 i       = head [i | ((si, i), (so, o)) <- t, si == sp, so == s]
+             in dc' t sp (dl bs) ++ [i]
 
 printT :: TrellisGraph -> IO ()
 printT []                    = return ()
